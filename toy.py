9777fb70cf0ba8496d3056fb974754a999315e14
john mangual <john.mangual@gmail.com>
2015-06-28 16:18:38 -0400

    bitwise operation on R unresolved...

--- toy.py
+++ toy.py
@@ -32,7 +32,7 @@ def dump(pc, R, mem):
 # the TOY simulator
 
 pc  = 0x10                      # program counter
-R   = [0 for x in range(16)]    # 16 registers
+R   = 0                         # 16 registers
 mem = [0 for x in range(256)]   # 256 main memory locations
 
 """
@@ -45,8 +45,10 @@ mem = [0 for x in range(256)]   # 256 main memory locations
 """
 
 # read the TOY program directly from the file
+#filename = input()
 
-f        = file(input())
+
+f        = file("toy.toy")
 #spaces + hexadecimal opcode : spaces/tabs hexadecimal code
 regexp  = r"^\s*([0-9A-Fa-f]{2}):[ \t]*([0-9A-Fa-f]{4}).*"
 pattern = re.compile(regexp)
@@ -55,8 +57,10 @@ print "Reading instructions:"
 print "---------------------"
 
 for line in f.readlines():
+    
     if(pattern.match( line )):
-        
+        #print pattern.match( line ).group(1)
+    
         addr = pattern.match( line ).group(1);
         inst = pattern.match( line ).group(2);
         print "%s: %s\n" % (addr, inst)
@@ -72,3 +76,91 @@ dump(pc, R, mem)
 
 print "Terminal"
 print "---------------------------------------"
+
+while True:
+    # Fetch and parse
+    pc += 1
+    inst = mem[pc]           # fetch next instruction
+    op   = (inst >> 12) &  15  # get opcode (bits 12-15)
+    d    = (inst >>  8) &  15  # get dest   (bits  8-11)
+    s    = (inst >>  4) &  15  # get s      (bits  4- 7)
+    t    = (inst >>  0) &  15  # get t      (bits  0- 3)
+    addr = (inst >>  0) & 255  # get addr   (bits  0- 7)
+
+    # halt
+    if (op == 0): break
+
+    # stdin 
+    if ((addr == 255 and op == 8) or (R[t] == 255 and op == 10)):
+        mem[255] = fromHex(input());
+
+    # Execute
+    if op == 1:
+        # add
+        R[d] = R[s] +  R[t] 
+    elif op == 2:
+        # subtract
+        R[d] = R[s] -  R[t] 
+    elif op == 3:
+        # bitwise and 
+        R[d] = R[s] &  R[t] 
+    elif op == 4:
+        # bitwise xor
+        R[d] = R[s] ^  R[t] 
+
+    elif op == 5:
+        # shift left
+        R[d] = (R[s] << R[t])%2**16 
+    elif op == 6:
+        # shift right
+        R[d] =  R[s] >> R[t]       
+    elif op == 7:
+        # load address
+        R[d] = addr         
+    elif op == 8:
+        # load
+        R[d] = mem[addr]    
+
+    elif op == 9:
+        # store
+        mem[addr] = R[d]       
+    elif op ==10:
+        # load indirect
+        R[d] = mem[R[t] & 255] 
+    elif op ==11:
+        # store indirect  
+        mem[R[t] & 255] = R[d]  
+    elif op ==12:
+        # branch if zero
+        if (R[d] % 2**15 == 0): pc = addr 
+            
+    elif op ==13:   
+        # branch if positive
+        if (R[d] / 2**15 >  0): pc = addr  
+    elif op ==14:
+        # jump indirect
+        pc = R[d]                         
+    elif op ==15:
+        # jump and link     
+        R[d] = pc
+        pc = addr                
+        
+    break
+
+# stdout
+if ((addr == 255 and op == 9) or (R[t] == 255 and op == 11)):
+    print toHex(mem[255])
+
+R[0] = 0                # ensure R0 is always 0
+R[d] = R[d] & 0xFFFF    # don't let R[d] overflow a 16-bit integer
+pc   = pc   & 0xFF      # don't let pc overflow an 8-bit integer
+
+"""
+*****************************************************
+*  Print out final contents of TOY.
+*****************************************************
+"""
+print 
+print "Core Dump of TOY After Executing"
+print "---------------------------------------"
+dump(pc, R, mem)
